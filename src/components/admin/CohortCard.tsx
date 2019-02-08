a4ae97ed455b9baef466711c927be55ca2e37b1f
009kings <009kings@gmail.com>
2019-02-07 16:00:22 -0800

    Here's the same thing it was doing before but with three new files

---
+++ src/components/admin/CohortCard.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Cohort } from '../../Types';
+
+// TODO: create and import ShowAllCohorts, ShowAllInstructors, and ShowAllStudents (the edit and delete functionality for those will live in thos components)
+
+
+const CohortCard = (props: Cohort) => {                                  
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default CohortCard;
