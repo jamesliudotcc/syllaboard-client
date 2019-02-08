c6e56a5ca1a43d7537cb0e89c2c9fc262b8aedf0
009kings <009kings@gmail.com>
2019-02-07 17:16:05 -0800

    Buttons and colours and cards Oh my!

--- src/components/admin/CohortCard.tsx
+++ src/components/admin/CohortCard.tsx
@@ -1,14 +1,69 @@
 import * as React from 'react';
 import { Cohort } from '../../Types';
+import { WithStyles, Theme, createStyles, withStyles } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import UiTheme from '../../style/theme';
 
-// TODO: create and import ShowAllCohorts, ShowAllInstructors, and ShowAllStudents (the edit and delete functionality for those will live in thos components)
+const styles = (theme: Theme) => createStyles({
+  card: {
+    display: 'flex',
+    margin: theme.spacing.unit
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+  },
+  edit: {
+    color: 'green'
+  },
+  delete: {
+    color: 'red'
+  }
+});
 
+type Props = Cohort & WithStyles<typeof styles>;
 
-const CohortCard = (props: Cohort) => {                                  
-  return (
-    <div>
-    </div>
-  );
+class CohortCard extends React.Component<Props, {}> {                  
+ 
+  render() {
+    return (
+      <Card className={this.props.classes.card}>
+        <div className={this.props.classes.details}>
+          <CardContent className={this.props.classes.content}>
+            <Typography component="h5" variant="h5">
+              {this.props.name}
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+              {this.props.instructors}
+            </Typography>
+          </CardContent>
+          <div className={this.props.classes.controls}>
+          {/* TODO Colour the buttons! */}
+          <IconButton className={this.props.classes.edit} aria-label="Edit">
+            <EditIcon />
+          </IconButton>
+          <IconButton className={this.props.classes.delete} aria-label="Delete">
+            <DeleteIcon />
+          </IconButton>
+          </div>
+        </div>
+      </Card>
+    );
+  }
 }
 
-export default CohortCard;
+export default withStyles(styles)(CohortCard);
