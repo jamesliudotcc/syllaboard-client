707bf6794bb24f9fe34d929780e74c77e632553e
009kings <009kings@gmail.com>
2019-02-08 15:19:47 -0800

    Spinners!

--- src/components/admin/ShowAllCohorts.tsx
+++ src/components/admin/ShowAllCohorts.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Cohort } from '../../Types';
 import CohortCard from './CohortCard';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 import { Theme, WithStyles, createStyles, withStyles } from '@material-ui/core';
 
@@ -20,11 +21,15 @@ type Props = OwnProps & WithStyles<typeof styles>;
 class ShowAllCohorts extends React.Component<Props, {}> {
   
   render () {
-    const cohortCards = this.props.cohorts.length < 1 ? <p>LOADING</p> : this.props.cohorts.map(cohort => (
-    <Grid item xs={12} sm={6}>
-      <CohortCard  key={cohort.instructorKey} {...cohort} />
-    </Grid>
-    ));
+    const cohortCards = this.props.cohorts.length < 1 
+    ? <Grid item>
+        <CircularProgress /> 
+      </Grid>
+    : this.props.cohorts.map(cohort => (
+        <Grid item xs={12} sm={6}>
+          <CohortCard  key={cohort.instructorKey} {...cohort} />
+        </Grid>
+      ));
 
     return (
       <Grid container spacing={24} justify="space-around"
