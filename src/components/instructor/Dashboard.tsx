e79a683ebc05aa8430ffceee8874637c4910b712
jamesliudotcc <james@jamesliu.cc>
2019-02-10 12:53:12 -0800

    At error where types for props from state are not being passed.

--- src/components/instructor/Dashboard.tsx
+++ src/components/instructor/Dashboard.tsx
@@ -1,28 +1,25 @@
-import Typography from "@material-ui/core/Typography";
-import * as React from "react";
-import { RouteComponentProps } from "react-router";
-import { Dispatch } from "redux";
-import * as instructorActions from "../../actions/instructor_dashboard";
-import { fetchMessage } from "../../actions/notifications";
-import { State } from "../../reducers";
+import Typography from '@material-ui/core/Typography';
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import { Dispatch } from 'redux';
+import * as instructorActions from '../../actions/instructor_dashboard';
+import { fetchMessage } from '../../actions/notifications';
+import { State } from '../../reducers';
 import {
   Assignment,
-  Cohort,
   Deliverable,
   NewAssignmentInfo,
-  NewCohortInfo,
   NewDeliverableInfo,
-  User
-} from "../../Types";
-import { connectedComponentHelper } from "../../utils/connectedComponent";
-import Cohorts from "./CohortPanel/Cohorts";
-import Assignments from "./AssignmentPanel/Assignments";
-import Deliverables from "./DeliverablePanel/Deliverables";
+} from '../../Types';
+import { connectedComponentHelper } from '../../utils/connectedComponent';
+import Assignments from './AssignmentPanel/Assignments';
+import Cohorts from './CohortPanel/Cohorts';
+// import Deliverables from "./DeliverablePanel/Deliverables";
 
 const mapStateToProps = (state: State) => ({
   message: state.notifications.message,
   errorMessage: state.auth.error,
-  ...state.instructorDashboard
+  ...state.instructorDashboard,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
@@ -57,7 +54,7 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
 
 const { propsGeneric, connect } = connectedComponentHelper<{}>()(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 );
 type ComponentProps = typeof propsGeneric;
 
@@ -72,39 +69,23 @@ class InstructorDashboard extends React.Component<Props, {}> {
   }
 
   render() {
-    const addAssignmentPanel = this.props.showAddAssignment ? (
-      <div>
-        {/* TODO: Add 'add assignment' component */}
-        Add Assignment Component Goes Here!
-      </div>
-    ) : (
-      <div />
-    );
 
-    const addDeliverablePanel = this.props.showAddDeliverable ? (
-      <div>
-        {/* TODO: Add 'add deliverable' component */}
-        Add Deliverable Component Goes Here!
-      </div>
-    ) : (
-      <div />
-    );
     const cohortData = {
       cohorts: this.props.cohorts,
       errorMessage: this.props.errorMessage,
-      ...this.props.cohort
+      ...this.props.cohort,
     };
     const assignmentData = {
       assignments: this.props.assignment,
       showAddAssignment: this.props.showAddAssignment,
       errorMessage: this.props.errorMessage,
-      ...this.props.assignment
+      ...this.props.assignment,
     };
     const deliverableData = {
       deliverables: this.props.deliverables,
       showAddDeliverable: this.props.showAddDeliverable,
       errorMessage: this.props.errorMessage,
-      ...this.props.deliverable
+      ...this.props.deliverable,
     };
 
     // TODO: remove after testing ^^^^^^^
@@ -114,9 +95,9 @@ class InstructorDashboard extends React.Component<Props, {}> {
         <Typography variant="h2" align="center">
           Instructor Dashboard
         </Typography>
-        <Cohorts {...cohortData} />
-        <Assignment {...assignmentData} />
-        <Deliverable {...deliverableData} />
+        {/* <Cohorts {...cohortData} /> */}
+        <Assignments {...assignmentData} />
+        {/* <Deliverable {...deliverableData} /> */}
       </div>
     );
   }
