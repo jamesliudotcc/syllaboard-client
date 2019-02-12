1127579ef0de3c32b5fd0772ab0215e7ac18a16f
Parker Couch <parkercouch@gmail.com>
2019-02-12 15:55:10 -0800

    Added better page not found page

--- src/components/NoMatch.tsx
+++ src/components/NoMatch.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+
+// Material UI imports
+import {
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles,
+} from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 
-const NoMatch = () => (
-  <div>
-    Page Not Found
-    <Link to="/">Go Back Home</Link>
-  </div>
-);
+const styles = (theme: Theme) =>
+  createStyles({
+    center: {
+      textAlign: 'center',
+    },
+    title: {
+      margin: '50px auto',
+      textAlign: 'center',
+    },
+  });
 
-export default NoMatch;
+type Props = WithStyles<typeof styles>;
+
+const NoMatch = (props: Props) => {
+  return (
+    <div className={props.classes.center}>
+      <Typography variant="h3" className={props.classes.title}>
+        Page Not Found
+      </Typography>
+      <br/>
+      <Link to="/">Go Back Home</Link>
+    </div>
+  );
+};
+
+export default withStyles(styles)(NoMatch);
